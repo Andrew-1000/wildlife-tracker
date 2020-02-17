0639c662dd3bb95024098a84aa361025a8a91abb
Andrew-1000 <andruambia95@gmail.com>
2020-02-17 08:57:38 +0000

    Add new entities to the wildlife.sql file in the database package

--- src/main/resources/database/wildlifetrack.sql
+++ src/main/resources/database/wildlifetrack.sql
@@ -1,6 +1,6 @@
 SET MODE PostgreSQL;
 
-CREATE TABLE IF NOT EXISTS ranger (ranger_id serial PRIMARY KEY, ranger_name varchar);
-CREATE TABLE IF NOT EXISTS animal (animal_id serial PRIMARY KEY, animal_name varchar, age varchar, endangered BOOLEAN);
+CREATE TABLE IF NOT EXISTS rangers (ranger_id int PRIMARY KEY, ranger_name varchar);
+CREATE TABLE IF NOT EXISTS animals (animal_id int PRIMARY KEY, animal_name varchar, health varchar, age varchar, endangered varchar);
 CREATE TABLE IF NOT EXISTS sightings (sighting_id int PRIMARY KEY, location varchar animal_id INTEGER,ranger_id INTEGER);
 
